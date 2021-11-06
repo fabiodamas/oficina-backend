c59d33cffd3c1b47d3fa3f9be138df8c0adbd67e
fabio.damas <fhdamas@PRODAM.SP.GOV.BR>
2021-11-06 05:34:08 -0300

    Initial commit

---
+++ src/main/java/io/github/fabiodamas/oficinabackend/OficinaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.fabiodamas.oficinabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/fabiodamas/oficinabackend/OficinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fabiodamas.oficinabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
